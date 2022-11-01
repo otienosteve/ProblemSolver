e0f5b464841b35c86fe5e3224ce45ff4f71488b0
otienosteve <steveotieno701@gmail.com>
2022-11-01 14:02:04 +0300

    Solve Problem 1

---
+++ twosum.js
@@ -0,0 +1,12 @@
+const  twoSum=(arr,sum)=>{
+const obj={}
+for (var f=0; f<=arr.length-1; f++){ 
+if(Object.keys(obj).map(n=>parseInt(n)).includes(sum-arr[f])){
+return [arr[f],arr[obj[sum-arr[f]]]]
+}
+obj[arr[f]]=f
+}
+return []
+
+}
+console.log(twoSum([3,5,-4,8,11,1,-1,6],10))
